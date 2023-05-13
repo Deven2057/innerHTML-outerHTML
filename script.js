94533597b473fe951f54eb3a4022ad7c65e90c9d
Devendra Bhattarai <91841677+Deven2057@users.noreply.github.com>
2023-05-13 15:02:51 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+console.dir(document.getElementsByTagName("span")[0])
+console.log(document.getElementsByTagName("span")[0])
+
+let first=document.getElementById('first')
+console.log(first.innerHTML)
+first.innerHTML="i am god of the entire world"
+console.log(first.innerHTML)
+console.log(first.outerHTML)
+
+console.log(document.body.textContent)
